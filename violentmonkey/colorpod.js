6f1bcf46f0ab608872c4090038a57e00f2065964
Dani Carrillo <daniel.carrillo@liferay.com>
2025-06-24 17:45:36 +0200

    Quick win color

--- violentmonkey/colorpod.js
+++ violentmonkey/colorpod.js
@@ -8,24 +8,32 @@
 // @description 9/19/2024, 1:51:51 PM
 // ==/UserScript==
 
-function setPodBackground(background) {
+function setPodBackground(background, quickWin) {
    var curSelector = document.querySelector("#main_navigation nav");
    if (curSelector && typeof background == "undefined") {
       curSelector.style.background="rgb(3, 54, 61)";
    } else {
       curSelector.style.background=background;
    }
+
+   if (curSelector && curSelector.firstElementChild && quickWin) {
+      curSelector.firstElementChild.style.color="yellow";
+      curSelector.firstElementChild.style.fill="yellow";
+   } else {
+      curSelector.firstElementChild.style.color="rgb(216, 220, 222)";
+      curSelector.firstElementChild.style.fill="rgb(216, 220, 222)";
+   }
 }
 
 
 function customCheckPod(ticketId) {
     var element = document.querySelector(".property_box[data-ticket-id='"+ticketId+"'] .ember-view.form_field.lesa-ui-form-field.lesa-ui-suborganization>div");
-
     if (element) {
+        var quickWin = document.querySelector("div.lesa-ui-priority[data-ticket-id='"+ticketId+"'] span.lesa-ui-quickwin") !== null;
         if (element.textContent === "Spain Pod C") {
-           setPodBackground("rgb(0 144 64)");
+           setPodBackground("rgb(0 144 64)", quickWin);
         } else if (element.textContent === "Spain Pod A") {
-           setPodBackground("rgb(16 192 192)");
+           setPodBackground("rgb(16 192 192)", quickWin);
         } else {
            setPodBackground();
         }
@@ -56,3 +64,4 @@ function customUpdateZendeskUI() {
 }
 
 setInterval(customUpdateZendeskUI, 1000);
+
